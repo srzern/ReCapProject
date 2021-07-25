80201e3387c046bf246c87540ef2fbd69dd797ba
sezer <sezerby65@gmail.com>
2021-07-25 21:20:26 +0300

    8. gün ödev 1 - 25.07.2021

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -16,9 +16,39 @@ namespace Business.Concrete
             _CarDal = carDal;
         }
 
+        public void Add(Car car)
+        {
+            if (car.Description.Length >= 2)
+            {
+                if (car.DailyPrice > 0)
+                {
+                    _CarDal.Add(car);
+                    Console.WriteLine("{0} Açıklamalı aracınız, {1} lira fiyatı ile eklenmiştir", car.Description, car.DailyPrice);
+                }
+                else
+                {
+                    Console.WriteLine("HATA! Aracın günlük fiyatı 0'dan büyük olmalı");
+                }
+            }
+            else
+            {
+                Console.WriteLine("HATA! Araç açıklaması minimum 2 karakter olmalıdır!");
+            }
+        }
+
         public List<Car> GetAll()
         {
-            return _CarDal.getAll();
+            return _CarDal.GetAll();
+        }
+
+       public List<Car> GetCarsByBrandId(int id)
+        {
+            return _CarDal.GetAll(p => p.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _CarDal.GetAll(p => p.ColorId == id);
         }
     }
 }
--- Console/Program.cs
+++ Console/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -8,11 +10,28 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
-            foreach (var car in carManager.GetAll())
+            CarManager carManager = new CarManager(new EfCarDal());
+            //            carManager.Add(new Car { Description = "Güzel Araç", BrandId = 1, ColorId = 2, DailyPrice = 110, ModelYear = 2002 });
+            //            carManager.Add(new Car { Description = "Fantastik, doktordan bir araç", BrandId = 1, ColorId = 1, DailyPrice = 600, ModelYear = 2021 }
+            //);
+            //            carManager.Add(new Car { Description = "Tertemiz elektrikli bir araç", BrandId = 2, ColorId = 1, DailyPrice = 200, ModelYear = 2014 });
+
+            Console.WriteLine("Markaya göre");
+            var carsByBrand = carManager.GetCarsByBrandId(1);
+            foreach (var car in carsByBrand)
             {
-                Console.WriteLine(car.Id +" :"+car.Description);
+                Console.WriteLine(car.Id + " " + car.Description);
             }
+            Console.WriteLine("Renge göre");
+            var carsByColorId = carManager.GetCarsByColorId(1);
+            foreach (var car in carsByColorId)
+            {
+                Console.WriteLine(car.Id + " " + car.Description);
+            }
+
+
+
+
         }
     }
 }
